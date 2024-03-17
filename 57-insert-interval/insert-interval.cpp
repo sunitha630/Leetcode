4cea42cbb4c941ff5809d9cb08039f8f20b6ddf9
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2024-03-17 13:37:56 +0530

    Time: 12 ms (53.18%) | Memory: 20.5 MB (29.62%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        stack<pair<int,int>>st;
-        vector<vector<int>>v;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        ios_base::sync_with_stdio(0);
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<intervals.size();i++){
-            if(i==0){
-                st.push({intervals[i][0],intervals[i][1]});
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        if(n==1) return intervals;
+        int i=0;
+        while(i<n-1){
+            if(intervals[i][1]>=intervals[i+1][0]){
+                int j=i+1,maxi=-1,mi;
+                for(;j<n;j++){
+                    //cout<<intervals[j-1][1]<<" "<<intervals[j][0]<<endl;
+                    if(maxi<intervals[j-1][1]){
+                        maxi=intervals[j-1][1];
+                        mi=j-1;
+                    }
+                    if(intervals[mi][1]<intervals[j][0]) break;
+                }
+                if(intervals[mi][1] >= intervals[i][1] and intervals[j-1][1]>intervals[mi][1]) ans.push_back({intervals[i][0] , intervals[j-1][1]});
+                else if(intervals[mi][1] >= intervals[i][1]) ans.push_back({intervals[i][0] , intervals[mi][1]});
+                else ans.push_back({intervals[i][0] , intervals[i][1]});
+                i=j;
             }
             else{
-                if(st.top().second>=intervals[i][0]){
-                    int m=min(st.top().first,intervals[i][0]);
-                    int ma=max(st.top().second,intervals[i][1]);
-                    st.pop();
-                    st.push({m,ma});
-                }
-                else{
-                    st.push({intervals[i][0],intervals[i][1]}); 
-                }
+                ans.push_back({intervals[i][0] , intervals[i][1]});
+                i++;
             }
         }
-        while(!st.empty()){
-            vector<int>res;
-            res.push_back(st.top().first);
-            res.push_back(st.top().second);
-            v.push_back(res);
-            st.pop();
-        }
-        sort(v.begin(),v.end());
-        return v;
+        int k=ans.size();
+        if(ans[k-1][1]<intervals[n - 1][1]) ans.push_back({intervals[n-1][0] , intervals[n-1][1]});
+        return ans; 
+    }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        return merge(intervals);
     }
 };
