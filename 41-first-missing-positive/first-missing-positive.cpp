dbdd5c318d79d10f5fa5d55ea9c5f22e40ac6058
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2024-03-26 21:53:15 +0530

    Time: 140 ms (5.12%) | Memory: 60.6 MB (11.54%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+      map<int,int>mp;
+      for(int i=0;i<nums.size();i++){
+          mp[nums[i]]++;
+      } 
+        for(int i=1;i<=nums.size();i++){
+            if(mp.find(i)==mp.end()){
+                return i;
+            }
+        }
+        return nums.size()+1;
+    }
+};
