e67be6276e0e7fc5e07591192a39aa8cba13f1bd
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2025-01-30 23:19:51 +0530

    Time: 3 ms (47.66%) | Memory: 8.5 MB (46.46%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+void recurse(int start,vector<int>&vec,int k){
+	if(vec.size()==1) return;
+	int index = (start+k-1)%vec.size();
+	vec.erase(vec.begin()+index);
+	recurse(index,vec,k);
+}
     int findTheWinner(int n, int k) {
-      vector<int>vec;
-      for(int i=0;i<n;i++){
-        vec.push_back(i);
-      }
-      int i=0;
-      while(vec.size()>1){
-        i=(i+k-1)%vec.size();
-        vec.erase(vec.begin()+i);
-        //i++;
-      }
-      return vec[0]+1;
+        vector<int>vec;
+	for(int i = 0; i < n ; i++){
+		vec.push_back(i+1);
+	}
+	recurse(0,vec,k);
+    return vec[0];
     }
 };
