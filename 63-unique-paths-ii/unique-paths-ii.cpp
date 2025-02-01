476da73dcab850c028a0aea7ccc2e7d2dcfeac86
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2025-02-01 22:32:00 +0530

    Time: 19 ms (4.55%) | Memory: 11.8 MB (55.01%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>>g(m,vector<int>(n,1));
+        if(obstacleGrid[0][0]==1) return 0;
+        g[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j] == 1){
+                    g[i][j]=0;
+                }
+                else{
+                    if(i==0){
+                        if(j!=0){
+                            g[i][j]=g[i][j-1];
+                        }
+                    }
+                    else if(j==0){
+                        if(i!=0){
+                            g[i][j]=g[i-1][j];
+                        }
+                    }
+                    else{
+                        g[i][j]=g[i-1][j]+g[i][j-1];
+                    }
+                }
+                cout<<g[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return g[m-1][n-1];
+    }
+};
