6a2c1256d9a0892073dfb10d444fc8aa1e9c1778
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2025-07-28 19:54:37 +0530

    Time: 38 ms (3.61%) | Memory: 11.9 MB (54.14%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,36 +1,58 @@
 class Solution {
 public:
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
-        vector<vector<int>>g(m,vector<int>(n,1));
-        if(obstacleGrid[0][0]==1) return 0;
-        g[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1){
+            return 0;
+        }
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        cout<<n<<" "<<m<<endl;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cout<<obstacleGrid[i][j]<<" ";
+                if(i==0 && j==0){
+                    
+                    dp[i][j] = 1;
+                }
+               else if(i==0 && j>0){
                 if(obstacleGrid[i][j] == 1){
-                    g[i][j]=0;
+                   dp[i][j]=0;
+
                 }
                 else{
-                    if(i==0){
-                        if(j!=0){
-                            g[i][j]=g[i][j-1];
-                        }
-                    }
-                    else if(j==0){
-                        if(i!=0){
-                            g[i][j]=g[i-1][j];
-                        }
-                    }
-                    else{
-                        g[i][j]=g[i-1][j]+g[i][j-1];
-                    }
+                    dp[i][j] = dp[i][j-1];
                 }
-               
+               }
+               else if(j==0 && i>0){
+                if(obstacleGrid[i][j] == 1){
+                   
+                   dp[i][j]=0;
+                }
+                else{
+                  
+                    dp[i][j] = dp[i-1][j];
+                }
+               }
+               else
+               {
+              
+                if(obstacleGrid[i][j]!=1){
+                   
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+                
+               }
             }
             cout<<endl;
         }
-        return g[m-1][n-1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return dp[n-1][m-1];
     }
 };
