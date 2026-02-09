ed11dfe272df0baa6c1ae759905c8f06b9946f28
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-02-09 18:52:14 +0530

    Time: 59 ms (22.92%) | Memory: 93.1 MB (6.61%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++; //{key: key's frequency}
+        }
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if(it->second>=2){
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
