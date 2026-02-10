77a119d2754db5be7260b55e17c192ecd71fe66b
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-02-10 19:01:17 +0530

    Time: 8 ms (19.31%) | Memory: 16.1 MB (5.06%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> p = {
-            {1000, "M"}, 
-            {900, "CM"}, 
-            {500, "D"}, 
-            {400, "CD"}, 
-            {100, "C"},
-            {90, "XC"},  
-            {50, "L"},   
-            {40, "XL"}, 
-            {10, "X"},   
-            {9, "IX"},
-            {5, "V"},    
-            {4, "IV"},  
-            {1, "I"}
+        map<int,string,greater<int>>mp={
+            {1000,"M"},
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"}
         };
         string res="";
-        for(int i=0;i<p.size();i++){
-            int d=num/p[i].first;
-            while(d){
-                res+=p[i].second;
-                d--;
+        for(auto it:mp){
+            int value=it.first; //value
+            string symb=it.second; //symbol
+
+            while(num>=value){
+                res+=symb;
+                num-=value;
             }
-            cout<<num<<" "<<res<<endl;
-            num=num%p[i].first;
+
         }
         return res;
     }
