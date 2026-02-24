3c0a5a79b83c1c63607fcd4ac9017c0034cf48c9
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-02-24 18:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (91.75%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,59 +11,23 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL){
-            return NULL;
-        }
-        int c=0;
+        int length=0;
         ListNode* temp=head;
-        ListNode* temp1=head;
         while(temp!=NULL){
-            c++;
+            length++;
             temp=temp->next;
         }
-        if(c == 1){
-          if(n==1){
-              return NULL;
-          }
-          return head;
+        int pos=length-n;
+        if(pos==0) return head->next;
+        temp=head;
+
+        for(int i=1;i<pos;i++){
+            temp=temp->next;
         }
-        if(c==2){
-            if(n==1){
-                head->next=NULL;
-                return head;
-            }
-            else if(n==2){
-                head=head->next;
-                return head;
-            }
-        }
-        if(n==1){
-            while(temp1->next->next!=NULL){
-                temp1=temp1->next;
-            }
-            temp1->next=NULL;
-            return head;
-        }
-        if(n==2){
-            while(temp1->next->next->next!=NULL){
-                temp1=temp1->next;
-            }
-            temp1->next=temp1->next->next;
-            return head;
-        }
-        if(c==n){
-            head=head->next;
-            return head;
-        }
-        int i;
-        for(i=0;i<(c-n-1);i++){
-            temp1=temp1->next;
-        }
-        //cout<<temp1->val;
-        temp1->next=temp1->next->next;
-        
+
+        temp->next=temp->next->next;
+
         return head;
 
-        
     }
 };
