900929fe11d981e7e6d65bbcc2f526f69206c48f
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-02-26 19:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.91%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,17 +11,15 @@
  */
 class Solution {
 public:
-    void preorder(vector<int>&res,TreeNode* root){
-        if(root==NULL) return;
-        res.push_back(root->val);
-        preorder(res,root->left);
-        preorder(res,root->right);
-    }
-
+   void preorder(TreeNode* root,vector<int>&res){
+    if(root==NULL) return;
+    res.push_back(root->val);
+    preorder(root->left,res);
+    preorder(root->right,res);
+   }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>res;
-        preorder(res,root);
+        preorder(root,res);
         return res;
-        
     }
 };
