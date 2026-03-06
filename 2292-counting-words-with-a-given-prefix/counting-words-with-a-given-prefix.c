7be816ed35fd44461b68a833a6caa8d607843451
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-03-06 13:29:22 +0530

    Time: 2 ms (12.50%) | Memory: 9.4 MB (5.00%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.c
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.c
@@ -1,14 +1,14 @@
 int prefixCount(char** words, int wordsSize, char* pref) {
-    int count = 0;
-    for(int i=0;i<wordsSize;i++){
-        bool flag=true;
-        for(int j=0;pref[j]!='\0';j++){
-            if(pref[j]!=words[i][j]){
-                flag=false;
+    int count =0;
+    for(int i=0 ; i<wordsSize ;i++){
+        int c=0;
+        for(int j=0 ; j<strlen(pref) ; j++){
+            if(words[i][j]!=pref[j]){
+                c=1;
                 break;
             }
         }
-        if(flag) count++;
+        if(c==0) count++;
     }
     return count;
 }
