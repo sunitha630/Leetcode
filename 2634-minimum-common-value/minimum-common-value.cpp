42fa5c78d5f7255ea33e76a07c96a312bf0c0122
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-05-19 12:10:47 +0530

    Time: 11 ms (24.45%) | Memory: 54.6 MB (30.11%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    bool isThere(int tar,vector<int>& nums1){
-        int l=0,h=nums1.size()-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums1[mid]==tar){
-                return true;
-            }
-            else if(nums1[mid]>tar){
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return false;
-    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        for(int i=0;i<nums2.size();i++){
-            if(isThere(nums2[i],nums1)){
-                return nums2[i];
+        int i=0,j=0;
+        int mi=1000000000;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
             }
         }
         return -1;
-
     }
 };
