2a57fa50f50c040729cb8c2819d23109235451d0
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-05-30 18:41:45 +0530

    Time: 3 ms (89.54%) | Memory: 23.9 MB (59.28%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
+        //start of first interval
+        int low=intervals[0][0];
+        //end of the first interval
+        int high=intervals[0][1];
         vector<vector<int>>res;
-        int low=intervals[0][0],high=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-           if(high>=intervals[i][0]){
+            if(intervals[i][0]<=high){
                 high=max(high,intervals[i][1]);
-           }
-           else{
+            }
+            else{
                 res.push_back({low,high});
                 low=intervals[i][0];
-                high=max(high,intervals[i][1]);
-           }
-
+                high=intervals[i][1];
+            }
         }
         res.push_back({low,high});
         return res;
+
     }
 };
