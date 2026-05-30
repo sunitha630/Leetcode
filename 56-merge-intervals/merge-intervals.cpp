2ededde1a65861a7133181e63ac9619f88367b8c
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-05-30 13:24:45 +0530

    Time: 4 ms (66.42%) | Memory: 23.9 MB (59.28%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-      stack<pair<int,int>>st;
-        vector<vector<int>>v;
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<intervals.size();i++){
-            if(i==0){
-                st.push({intervals[i][0],intervals[i][1]});
-            }
-            else{
-                if(st.top().second>=intervals[i][0]){
-                    int m=min(st.top().first,intervals[i][0]);
-                    int ma=max(st.top().second,intervals[i][1]);
-                    st.pop();
-                    st.push({m,ma});
-                }
-                else{
-                    st.push({intervals[i][0],intervals[i][1]}); 
-                }
-            }
+        vector<vector<int>>res;
+        int low=intervals[0][0],high=intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+           if(high>=intervals[i][0]){
+                high=max(high,intervals[i][1]);
+           }
+           else{
+                res.push_back({low,high});
+                low=intervals[i][0];
+                high=max(high,intervals[i][1]);
+           }
+
         }
-        while(!st.empty()){
-            vector<int>res;
-            res.push_back(st.top().first);
-            res.push_back(st.top().second);
-            v.push_back(res);
-            st.pop();
-        }
-        return v;
+        res.push_back({low,high});
+        return res;
     }
 };
