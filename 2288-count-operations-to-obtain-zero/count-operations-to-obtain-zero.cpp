a545f96dc4cfe27be4eb25e391a52b8c3a40e095
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-05-30 14:44:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.25%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int res=0;
+       while(num1 && num2){
+            if(num1<num2){
+                num2=num2-num1;
+            }
+            else{
+                num1=num1-num2;
+            }
+            res++;
+       } 
+       return res;
+    }
+};
