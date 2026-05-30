95e99996f23a14fcda71201adda12e92a864332f
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-05-30 19:19:31 +0530

    Time: 48 ms (52.44%) | Memory: 94 MB (45.96%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    static bool compare(vector<int>& a , vector<int>& b) {
-    return a[1] < b[1];
-    }
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end(),compare);
-        int c=points[0][1];
-        int arrows=1;
+        sort(points.begin(),points.end());
+        int low=points[0][0];
+        int high=points[0][1];
+        int arrows_cnt=1;
         for(int i=1;i<points.size();i++){
-            if(points[i][0]>c){
-                arrows++;
-                c=points[i][1];
+            //overlap
+            if(points[i][0]<=high){
+                low=max(low,points[i][0]);
+                high=min(high,points[i][1]);
+            }
+            //non-overlap
+            else{
+                arrows_cnt++;
+                low=points[i][0];
+                high=points[i][1];
             }
         }
-        return arrows;
+        return arrows_cnt;
     }
 };
