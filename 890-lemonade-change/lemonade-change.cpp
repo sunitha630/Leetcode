8656a0ef95550ab46189a1c2598da259a3cd3fcb
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-05-30 18:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (74.57%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five_cnt=0,ten_cnt=0,twen_cnt=0;
+        int five_cnt=0,ten_cnt=0,twenty_cnt=0;
         for(int i=0;i<bills.size();i++){
-            if(bills[i]==5){
+            if(bills[i] == 5){
                 five_cnt++;
             }
-        else if(bills[i]==10){
-            ten_cnt++;
-            if(five_cnt>=1){
-                five_cnt--;
+            else if(bills[i] == 10){
+                if(five_cnt>=1){
+                    five_cnt--;
+                }
+                else{
+                    return false;
+                }
+                ten_cnt++;
             }
             else{
-              
-                return false;
+                if(ten_cnt>=1 && five_cnt>=1){
+                    ten_cnt--;
+                    five_cnt--;
+
+                }
+                else if(five_cnt>=3){
+                    five_cnt-=3;
+                }
+                else{
+                    return false;
+                }
+                twenty_cnt++;
             }
-            
-        }
-        else{
-             if(ten_cnt>=1 && five_cnt>=1){
-                ten_cnt--;
-                five_cnt--;
-            }
-            else if(five_cnt>=3){
-                five_cnt-=3;
-            }
-            else{
-               
-                return false;
-            }
-            twen_cnt++;
-        }
         }
         return true;
     }
