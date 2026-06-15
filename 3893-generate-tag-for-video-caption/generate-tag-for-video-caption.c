c7f9b2822909fb6128cfb669a1c3e63855ec0dda
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-06-15 16:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (93.75%) - LeetSync

--- 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.c
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.c
@@ -1,52 +1,41 @@
 char* generateTag(char* caption) {
     static char res[101];
-    int i = 0;
+    res[100]='\0';
     res[0]='#';
-    res[100] = '\0';
-    int j=1;
-    while(caption[i] == ' ')i++;
-    bool isStart = true;
-    for(    ;caption[i]!='\0';i++){
-        if(j == 100) return res;
-        if(i == 0 || isStart) {
-            isStart = false;
-            if(caption[i] >= 'A' && caption[i] <= 'Z') {
-                res[j++] = 'a' + caption[i]-'A';
-            }else{
-                res[j++] = caption[i];
-            }
-        }else if(caption[i-1] == ' ' && caption[i] != ' ') {
-            if(caption[i] >= 'a' && caption[i] <= 'z') {
-                res[j++] = 'A' + caption[i]-'a';
-            }else {
-                res[j++] = caption[i];
-            }
-        }else if(caption[i] != ' ') {
-            if(caption[i] >= 'A' && caption[i] <= 'Z') {
-                res[j++] = 'a' + caption[i]-'A';
-            }else{
-                res[j++] = caption[i];
-            }
-        }   
-    } 
-    res[j] = '\0';
-    return res;
-}
-/*
-if(caption[i]!=' '){
-        ra=true;
-    }
-    else{
-        if(ra){
-            ra=false;
-            res[j++]=caption[i]-32;
-            
+    int i=0,j=1;
+    bool flag=false;
+    while(j<100 && caption[i]!='\0'){
+        if(caption[i]==' '){
+            flag=true;
         }
         else{
-            res[j++]=caption[i];
+            if(flag){
+                if(caption[i]>='a' && caption[i]<='z'){
+                  res[j++]=caption[i]-32;
+                }
+                else{
+                    res[j++]=caption[i];
+                }
+                flag=false;
+            }
+            else{
+                if(caption[i]>='A' && caption[i]<='Z'){
+                     res[j++]=caption[i]+32;
+                }
+                else{
+                    res[j++]=caption[i];
+                }
+            }
         }
-        if(j==100){
-            return res;
-        }
+        i++;
     }
+    if(res[1]>='A' && res[1]<='Z'){
+        res[1]=res[1]+32;
+    }
+    res[j]='\0';
+    return res;
+    
+}
+/*
+
 */
