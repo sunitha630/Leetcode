cbb597ba65eeb010b09e52b88d8b8930cefce726
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2026-08-05 10:31:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.61%) - LeetSync

--- 202-happy-number/happy-number.c
+++ 202-happy-number/happy-number.c
@@ -1,17 +1,17 @@
-int squaresOfSum(int n){
-    int sum=0;
-    while(n){
-        sum += ((n % 10)*(n%10));
-        n /= 10;
+int sumOfSquaresOfDigits(int num){
+    int sum = 0;
+    while(num!=0){ 
+        int digit = num % 10; 
+        sum += (digit*digit); 
+        num/=10;
     }
-    return sum;
+    return sum; 
 }
 bool isHappy(int n) {
-    if(n==1) return true;
-    int temp=n;
-    while(temp>=10){
-        temp=squaresOfSum(temp);
+    while(n>=10){
+    int ans=sumOfSquaresOfDigits(n);
+    n=ans;
     }
-    if(temp==1 || temp==7) return true;
+    if(n==1 || n==7) return true;
     return false;
 }
